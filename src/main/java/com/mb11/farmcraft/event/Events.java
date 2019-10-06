e46a2271be3aef2fc7d51fafcffe13a31fe725de
Will Toll <will@jtoll.com>
2019-10-06 15:40:34 -0600

    Rewrote Bee Suit Logic
    Streamlined the Bee Suit logic so it utilizes lambda expressions

--- src/main/java/com/mb11/farmcraft/event/Events.java
+++ src/main/java/com/mb11/farmcraft/event/Events.java
@@ -1,8 +1,10 @@
 package com.mb11.farmcraft.event;
 
+import com.mb11.farmcraft.armor.CustomArmorMaterials;
 import com.mb11.farmcraft.item.Items;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.BeeEntity;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -18,41 +20,19 @@ public class Events {
 
     public static void register() {
         PlayerTickCallback.EVENT.register((player) -> {
-
-            boolean helmet = false;
-            boolean chestplate = false;
-            boolean leggings = false;
-            boolean boots = false;
-
-            for (int i = 0; i < player.inventory.armor.size(); i++) {
-                ItemStack item = player.inventory.armor.get(i);
-                if (item.getItem() == Items.bee_mask) {
-                    helmet = true;
-                }
-                if (item.getItem() == Items.bee_jacket) {
-                    chestplate = true;
-                }
-                if (item.getItem() == Items.bee_pants) {
-                    leggings = true;
-                }
-                if (item.getItem() == Items.bee_boots) {
-                    boots = true;
-                }
-            }
-
-            if (helmet && chestplate && leggings && boots) {
-                List<Entity> entities = player.getEntityWorld().getEntities(player, new Box(player.getBlockPos().add(16, 16, 16), player.getBlockPos().add(-16, -16, -16)));
-                for (int i = 0; i < entities.size(); i++) {
-                    Entity entity = entities.get(i);
-                    if (entity instanceof BeeEntity) {
-                        BeeEntity bee = ((BeeEntity) entity);
-                        if (bee.getAttacker() == player) {
-                            bee.setAnger(0);
+            player.inventory.armor.forEach(item -> {
+                if (!((ArmorItem) item.getItem()).getMaterial().equals(CustomArmorMaterials.BEESUIT)) {
+                    return;
+                } else {
+                    player.getEntityWorld().getEntities(player, new Box(player.getBlockPos().add(16, 16, 16), player.getBlockPos().add(-16, -16, -16))).forEach(entity -> {
+                        if (entity instanceof BeeEntity) {
+                            if (((BeeEntity) entity).getAttacker() == player) {
+                                ((BeeEntity) entity).setAnger(0);
+                            }
                         }
-                    }
+                    });
                 }
-
-            }
+            });
             return ActionResult.PASS;
         });
 
